042e44f5ab59edda9bfc33ac18a46b7498bd6aaf
Aaron Clark <aaronpclark92@gmail.com>
2019-10-08 15:06:34 -0500

    Created TodoList.js, TodoForm.js, and TodoItem.js components

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const TodoForm = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Import Reducer
+
+export const TodoList = () => {
+
+
+  return (
+    <div>
+      {/* TodoForm.js share state */}
+      {/* Map over List to render TodoItem */}
+    </div>
+  )
+}
